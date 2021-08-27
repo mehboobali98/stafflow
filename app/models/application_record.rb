d023238f1ea1e4ae97e9cfc2a3e298d0cff6a04f
Mehboob Ali <FastNucesl174316>
2021-08-27 07:45:10 +0500

    added multitenancy logic in application_record.rb

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,25 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.inherited(subclass)
+    super
+
+    subclass.instance_eval do
+      def set_not_multitenant
+        @not_multitenant = true
+      end
+
+      def check_if_multitenant?
+        @not_multitenant.nil?
+      end
+    end
+
+    trace = TracePoint.new(:end) do |tp|
+      if tp.self == subclass && tp.self.check_if_multitenant?
+        trace.disable
+        subclass.instance_eval { default_scope { where(company_id: Company.current_company_id) } }
+      end
+    end
+    trace.enable
+  end
 end
