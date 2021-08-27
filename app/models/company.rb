e10be6906e86016aa69667865d1dd015493434e4
Mehboob Ali <FastNucesl174316>
2021-08-27 08:11:12 +0500

    made company.rb not multitenant, used Thread.current

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,14 @@
 class Company < ApplicationRecord
+  set_not_multitenant
+
+  def self.current_company_id=(company_id)
+    Thread.current[:current_company_id] = company_id
+  end
+
+  def self.current_company_id
+    Thread.current[:current_company_id]
+  end
+
   def self.find_company_by_subdomain(subdomain)
     Company.find_by_subdomain! subdomain
   end
