338243754140f7e4947870d4290d0aa7c9354f42
Mehboob Ali <FastNucesl174316>
2021-08-27 07:42:24 +0500

    added company related methods in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  around_action :set_current_company
+
+  def current_company
+    Company.find_company_by_subdomain(request.subdomain)
+  end
+
+  def set_current_company
+    Company.current_company_id = current_company.id
+    yield
+  rescue ActiveRecord::RecordNotFound
+    redirect_to '/?NoRecordFound'
+  ensure
+    Company.current_company_id = nil
+  end
+
+  private :current_company, :set_current_company
 end
