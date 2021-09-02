2ddd7d9b77e9eabcb393edd9409e2ceca59d01a4
Abdul Basit <abdul.basit@7vals.com>
2021-09-02 17:19:59 +0500

    Deleted company migration files I created for testing purposes only

--- db/migrate/20210831083726_add_name_to_company.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToCompany < ActiveRecord::Migration[6.0]
-  def change
-    add_column :companies, :name, :string
-  end
-end
--- db/migrate/20210831102640_add_subdomain_to_companies.rb
+++
@@ -1,5 +0,0 @@
-class AddSubdomainToCompanies < ActiveRecord::Migration[6.0]
-  def change
-    add_column :companies, :subdomain, :string
-  end
-end
