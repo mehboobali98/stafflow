f2dc9783c0c4d7d64a7c83e820f296403efe102d
Mehboob Ali <FastNucesl174316>
2021-09-02 12:10:51 +0500

    CRUD for leave type completed

--- app/controllers/leave_types_controller.rb
+++ app/controllers/leave_types_controller.rb
@@ -10,17 +10,45 @@ class LeaveTypesController < ApplicationController
     @leave_type = LeaveType.new
   end
 
-  def create; end
+  def create
+    @leave_type = LeaveType.new(leave_type_params)
+    if @leave_type.save
+      flash[:notice] = 'Leave type created successfully'
+      redirect_to action: 'index'
+    else
+      flash[:alert] = 'Unable to create leave type'
+      render :new
+    end
+  end
 
   def edit; end
 
-  def update; end
+  def update
+    if @leave_type.update(leave_type_params)
+      flash[:notice] = 'Leave type updated successfully'
+      redirect_to action: 'index'
+    else
+      flash[:alert] = 'Unable to update leave type'
+      render :edit
+    end
+  end
 
-  def destroy; end
+  def destroy
+    if @leave_type.destroy
+      flash[:notice] = 'Leave type deleted successfully'
+    else
+      flash[:alert] = 'Unable to delete leave type'
+    end
+    redirect_to action: 'index'
+  end
 
   private
 
   def set_leave_type
     @leave_type = LeaveType.find(params[:id])
+  end
+
+  def leave_type_params
+    params.require(:leave_type).permit(:name, :count)
   end
 end
