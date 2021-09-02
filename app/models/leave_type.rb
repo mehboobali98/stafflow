7440e3d890ab19bb02b1dda979dc222a2f059680
Mehboob Ali <FastNucesl174316>
2021-09-02 19:13:49 +0500

    leave type model

--- app/models/leave_type.rb
+++ app/models/leave_type.rb
@@ -1,2 +1,9 @@
 class LeaveType < ApplicationRecord
+  validates :name, format: { with: /\A[a-z A-Z]+\z/,
+                             message: 'Leave type name can only contain letters' }
+
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :count, numericality: { only_integer: true }
+  validates :count, length: { in: 1..20 }
+  validates :name, :count, presence: true
 end
