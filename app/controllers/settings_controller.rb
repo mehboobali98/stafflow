a6d2590d34d6f58b8bae65eb523f291b559b170e
Abdul Basit <abdul.basit@7vals.com>
2021-09-06 17:30:45 +0500

    Company settings crud final

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,21 +1,54 @@
 class SettingsController < ApplicationController
+  before_action :load_and_validate_settings, only: %I[show edit update]
+
+  # GET /settings/new
   def new
     @setting = Setting.new
   end
 
+  # GET /settings
+  def index
+    @setting = Setting.all
+  end
+
+  # POST /settings
   def create
-    binding.pry
     @setting = Setting.new(permit_settings_parameters)
-    if @setting.save
-      redirect_to root_url, notice: 'Settings were updated'
-    else
-      render 'new'
+    respond_to do |format|
+      if @setting.save
+        format.html { redirect_to @setting, notice: 'Successfully Saved' }
+      else
+        format.html { render :new, notice: 'Not Saved' }
+      end
     end
   end
+
+  # GET /settings/:id/edit
+  def edit; end
+
+  # PATCH /settings/:id
+  def update
+    respond_to do |format|
+      if @setting.update(permit_settings_parameters)
+        format.html { redirect_to @setting, notice: 'Successfully updated' }
+      else
+        format.html { render :edit, notice: 'Not updated' }
+      end
+    end
+  end
+
+  # GET /settings/:id
+  def show; end
 
   private
 
   def permit_settings_parameters
-    params.require(:setting).permit(:tax, :currency, :theme)
+    params.require(:setting).permit(:tax, :theme)
+  end
+
+  def load_and_validate_settings
+    @setting = Setting.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to settings_path, alert: 'Settings not found'
   end
 end
