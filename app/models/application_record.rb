c179362dccb7f04e52c81a5d3e0592f90a970615
NadiaAhsan <nadia.ahsan@7vals.com>
2021-09-06 15:25:43 +0500

    Space Issue Resolved

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,25 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  def self.inherited(subclass)
-    super
-
-    subclass.instance_eval do
-      def set_not_multitenant
-        @not_multitenant = true
-      end
-
-      def multitenant?
-        @not_multitenant.nil?
-      end
-    end
-
-    trace = TracePoint.new(:end) do |trace_point|
-      if trace_point.self == subclass && trace_point.self.multitenant?
-        trace.disable
-        subclass.instance_eval { default_scope { where(company_id: Company.current_company_id) } }
-      end
-    end
-    trace.enable
-  end
 end
