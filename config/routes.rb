0a3bf0b2329093bc8667964f9ffd316885f77eb2
Mehboob Ali <FastNucesl174316>
2021-09-06 14:16:24 +0500

    added display_calendar as a collection resource route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'displaycalendar', to: 'events#display_calendar'
-  resources :events
+  #get 'displaycalendar', to: 'events#display_calendar'
+  resources :events do
+    collection do
+      get 'display_calendar'
+    end
+  end
   root to: 'home#index'
   get '/home', to: 'home#home'
 end
