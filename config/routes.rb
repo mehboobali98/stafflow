bece5b4b9ac5a0f49e9deb6c53bc8aa4b3d5049c
Abdul Basit <abdul.basit@7vals.com>
2021-09-07 10:46:46 +0500

    Resolved problem with routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,9 @@ require_relative 'initializers/subdomain_validator'
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root to: 'home#index'
-  get '/home', to: 'home#home'
   resources :members, controller: 'users'
 
   constraints(SubdomainValidator) do
-    root to: 'home#index'
     get '/home', to: 'home#home'
   end
 end
