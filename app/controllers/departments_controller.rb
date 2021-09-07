21121925249b56407e73b1a13c46406b70125341
NadiaAhsan <nadia.ahsan@7vals.com>
2021-09-07 11:57:42 +0500

    Respond to added

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class DepartmentsController < ApplicationController
+  before_action :find_department_with_id, only: %I[edit update destroy]
   def index
     @departments = Department.all
   end
@@ -10,47 +11,52 @@ class DepartmentsController < ApplicationController
   end
 
   def create
-    if Department.exists?(permitted_department_params)
-      flash[:notice] = I18n.t 'department.already_exist'
-    else
-      @department = Department.new(permitted_department_params)
-      if @department.save
-        flash[:notice] = I18n.t 'department.created'
+    respond_to do |format|
+      if Department.exists?(permitted_department_params)
+        flash[:notice] = I18n.t 'department.already_exist'
       else
-        flash[:alert] = I18n.t 'department.not_created'
+        @department = Department.new(permitted_department_params)
+        if @department.save
+          flash[:notice] = I18n.t 'department.created'
+        else
+          flash[:alert] = I18n.t 'department.not_created'
+        end
       end
+      format.html { redirect_to action: 'index' }
     end
-    redirect_to action: 'index'
   end
 
-  def edit
-    @department = Department.find(params[:id])
-  end
+  def edit; end
 
   def update
-    if Department.exists?(permitted_department_params)
-      flash[:notice] = I18n.t 'department.already_exist'
-    else
-      @department = Department.find(params[:id])
-      if @department.update(permitted_department_params)
+    respond_to do |format|
+      if Department.exists?(permitted_department_params)
+        flash[:notice] = I18n.t 'department.already_exist'
+      elsif @department.update(permitted_department_params)
         flash[:notice] = I18n.t 'department.updated'
       else
         flash[:alert] = I18n.t 'department.not_updated'
       end
+      format.html { redirect_to action: 'index' }
     end
-    redirect_to action: 'index'
   end
 
   def destroy
-    @department = Department.find(params[:id])
     @department.destroy
-    flash[:notice] = I18n.t 'department.destroy'
-    redirect_to action: 'index'
+    redirect_to action: 'index', notice: I18n.t('department.destroy')
   end
 
   private
 
   def permitted_department_params
     params.require(:department).permit(:name, :image_url)
+  end
+
+  def find_department_with_id
+    @department = Department.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    respond_to do |format|
+      format.html { redirect_to action: 'index', alert: I18n.t('department.not_exist') }
+    end
   end
 end
--- app/controllers/designations_controller.rb
+++ app/controllers/designations_controller.rb
@@ -1,48 +1,47 @@
 # frozen_string_literal: true
 
 class DesignationsController < ApplicationController
+  before_action :find_designation_with_id, only: %I[edit update destroy]
   def index
     @designations = Designation.all
   end
 
   def create
-    if Designation.exists?(permitted_designation_params)
-      flash[:notice] = I18n.t 'designation.already_exist'
-    else
-      @designation = Designation.new(permitted_designation_params)
-      if @designation.save
-        flash[:notice] = I18n.t 'designation.created'
+    respond_to do |format|
+      if Designation.exists?(permitted_designation_params)
+        flash[:notice] = I18n.t 'designation.already_exist'
       else
-        flash[:alert] = I18n.t 'designation.not_created'
+        @designation = Designation.new(permitted_designation_params)
+        if @designation.save
+          flash[:notice] = I18n.t 'designation.created'
+        else
+          flash[:alert] = I18n.t 'designation.not_created'
+        end
       end
+      format.html { redirect_to action: 'index' }
     end
-    redirect_to action: 'index'
   end
 
   def edit
-    @designation = Designation.find(params[:id])
     @departments = Department.all
   end
 
   def update
-    if Designation.exists?(permitted_designation_params)
-      flash[:notice] = I18n.t 'designation.already_exist'
-    else
-      @designation = Designation.find(params[:id])
-      if @designation.update(permitted_designation_params)
+    respond_to do |format|
+      if Designation.exists?(permitted_designation_params)
+        flash[:notice] = I18n.t 'designation.already_exist'
+      elsif @designation.update(permitted_designation_params)
         flash[:notice] = I18n.t 'designation.updated'
       else
         flash[:alert] = I18n.t 'designation.not_updated'
       end
+      format.html { redirect_to action: 'index' }
     end
-    redirect_to action: 'index'
   end
 
   def destroy
-    @designation = Designation.find(params[:id])
     @designation.destroy
-    flash[:notice] = I18n.t 'designation.destroy'
-    redirect_to action: 'index'
+    redirect_to action: 'index', notice: I18n.t('designation.destroy')
   end
 
   def new
@@ -54,5 +53,13 @@ class DesignationsController < ApplicationController
 
   def permitted_designation_params
     params.require(:designation).permit(:designation_name, :department_id)
+  end
+
+  def find_designation_with_id
+    @designation = Designation.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    respond_to do |format|
+      format.html { redirect_to action: 'index', alert: I18n.t('designation.not_exist') }
+    end
   end
 end
