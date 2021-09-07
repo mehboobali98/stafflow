365594766c89f29fbf70c2f80b7f58a160205805
MShehryarKhan <shehryar.khan@7vals.com>
2021-09-07 16:01:04 +0500

    benefit and payroll module

--- test/controllers/employee_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class EmployeeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/payrolls_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class PayrollsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
