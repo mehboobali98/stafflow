5075b723ac4101e8f6343fad03549456b196dff9
Mehboob Ali <FastNucesl174316>
2021-09-08 11:30:53 +0500

    remove extra file

--- app/javascript/packs/user_leaves/user_leave.js
+++
@@ -1,9 +0,0 @@
-$(document).on('change', '[type=checkbox]', function(e) {
-  let number_field_id = e.target.id.split('_').slice(0, 2).join("_").concat("_count");
-  if (this.checked) //when check box is checked
-  {
-    $("#".concat(number_field_id)).attr("disabled", false);
-  } else {
-    $("#".concat(number_field_id)).attr("disabled", true);
-  }
-});
