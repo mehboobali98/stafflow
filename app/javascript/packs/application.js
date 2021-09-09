fd5ff5ceab20826eafddeb2379d31b6ef705fbb7
Abdul Basit <abdul.basit@7vals.com>
2021-09-09 13:20:19 +0500

    PR comments resolved

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
 global.$ = global.jQuery = require("jquery"); 
 require("bootstrap")
 document.addEventListener("turbolinks:load", function() {
