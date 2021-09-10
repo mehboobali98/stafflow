92f88df9a7f7c3f3d523428c385c65c049d93f4a
NadiaAhsan <nadia.ahsan@7vals.com>
2021-09-10 14:03:11 +0500

    Msgs added in en.yml

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,13 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: %i[display_companies]
 
+  # POST /display_companies
   def display_companies
     User.unscoped do
       @total_companies = Company.joins(:users).where(users: { email: params[:user][:email] }).load
+    end
+    respond_to do |format|
+      format.html
     end
   end
 end
