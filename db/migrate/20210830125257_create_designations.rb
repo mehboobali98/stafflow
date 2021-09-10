fa7a3b709634735fa0dd01eaff4d8558c7e62a19
NadiaAhsan <nadia.ahsan@7vals.com>
2021-09-10 19:49:07 +0500

    company id added

--- db/migrate/20210830125257_create_designations.rb
+++ db/migrate/20210830125257_create_designations.rb
@@ -4,7 +4,7 @@ class CreateDesignations < ActiveRecord::Migration[6.0]
   def change
     create_table :designations do |t|
       t.string :designation_name
-
+      t.bigint :company_id
       t.timestamps
     end
   end
--- db/migrate/20210831074706_add_department_to_designation.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddDepartmentToDesignation < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :designations, :department, null: false
-  end
-end
