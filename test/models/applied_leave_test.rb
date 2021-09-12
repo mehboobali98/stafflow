26f5bef135ce1981bea0edde18ba2766930700e4
Mehboob Ali <FastNucesl174316>
2021-09-12 16:38:26 +0500

    remove unnecessary files

--- test/models/applied_leave_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AppliedLeaveTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/user_leave_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UserLeaveTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
