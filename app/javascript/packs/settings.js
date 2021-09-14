5d4e3b8c54a8be81200fa8e04fbd6fe74d406899
Ali Adnan Aslam <52164631+aliadnanaslam7@users.noreply.github.com>
2021-09-14 17:08:20 +0500

    Update app/javascript/packs/settings.js

--- app/javascript/packs/settings.js
+++ app/javascript/packs/settings.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $('#setting_form').on('change keyup', function() {
-    $('#submit_button').prop('disabled', false)
+    $('#submit_button').prop('disabled', false);
   });
 });
