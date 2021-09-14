42613276ea7a29eab87a4a95d0599355ba0ef055
Abdul Basit <abdul.basit@7vals.com>
2021-09-14 22:04:03 +0500

    95% done

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,7 @@
 class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
+  # GET /notifications
   def fetch_user_notifications
     @notifications = current_user.notifications
     respond_to do |format|
