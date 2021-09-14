086ec94512f3c8f551d17d9a3a2bc74025839842
NadiaAhsan <nadia.ahsan@7vals.com>
2021-09-14 16:33:49 +0500

    comments resolved

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,7 @@ class HomeController < ApplicationController
   # POST /display_companies
   def display_companies
     User.unscoped do
-      binding.pry
-      @companies = Company.joins(:users).where(users: (home_params )).load # If Load not used, then in templte, default scope applied and query changed.
+      @companies = Company.joins(:users).where(users: { email: home_params[:email] }).load # If Load not used, then in templte, default scope applied and query changed.
     end
     respond_to do |format|
       format.html
