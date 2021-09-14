1efb2fec321765546d541d98508e08fa57b98637
NadiaAhsan <nadia.ahsan@7vals.com>
2021-09-14 23:22:58 +0500

    minor changings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
-  binding.pry
   skip_before_action :authenticate_user!, only: %i[display_companies index]
   layout 'landing'
 
