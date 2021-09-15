1cebaa25d6f6f0de7e43236162a89927beed8a33
Abdul Basit <abdul.basit@7vals.com>
2021-09-15 22:58:25 +0500

    Done with Auth UI

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations'
   }
   resources :home do
     collection do
