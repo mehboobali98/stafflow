09c2a94f353efc51883d4d910bc0cff80de958f7
Mehboob Ali <FastNucesl174316>
2021-09-17 17:22:42 +0500

    resolving comments

--- app/models/concerns/event_abilities.rb
+++ app/models/concerns/event_abilities.rb
@@ -4,10 +4,10 @@ module EventAbilities
 
     if user.account_owner? || user.hr? || user.department_head?
       can :manage, Event, company_id: user.company_id
-      can :display_calendar, Event, company_id: user.company_id
+      #can :display_calendar, Event, company_id: user.company_id
     elsif user.employee?
       can :read, Event, company_id: user.company_id
-      can :display_calendar, Event, company_id: user.company_id
+      #can :display_calendar, Event, company_id: user.company_id
     end
   end
 end
