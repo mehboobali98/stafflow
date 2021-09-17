1cb96b71315969043de99d4f972a52287062be23
MShehryarKhan <shehryar.khan@7vals.com>
2021-09-17 11:58:58 +0500

    some PR changes resolved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,7 +72,7 @@ class UsersController < ApplicationController
   private
 
   def permit_user_params
-    params.require(:user).permit(:first_name, :email, :last_name, :date_of_birth, :department_id, :password, :password_confirmation, :role_id, :salary)
+    params.require(:user).permit(:first_name, :email, :last_name, :date_of_birth, :department_id, :password, :password_confirmation, :role_id, :base_salary)
   end
 
   def find_user
