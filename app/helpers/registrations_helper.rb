fb3c7554ad3eb531a40674d7b8c47c338886c6fe
Abdul Basit <abdul.basit@7vals.com>
2021-09-17 17:28:29 +0500

    Done

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,6 +1,9 @@
 module RegistrationsHelper
   def portal_domain
-    return "https://<span id='span_subdomain'></span>.teamabc.com".html_safe if Rails.env.development?
-    "https://<span id='span_subdomain'></span>.teamabc.com".html_safe
+    if Rails.env.development? 
+      "https://<span id='span_subdomain'></span>.teamabc.com".html_safe
+    else
+      "https://<span id='span_subdomain'></span>.teamabc.com".html_safe
+    end
   end
 end
