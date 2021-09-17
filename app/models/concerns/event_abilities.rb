4f92165abb88477aeb5754391cfe4c4dc373732c
Mehboob Ali <FastNucesl174316>
2021-09-17 08:10:37 +0500

    event abilities simplified

--- app/models/concerns/event_abilities.rb
+++ app/models/concerns/event_abilities.rb
@@ -2,13 +2,7 @@ module EventAbilities
   def define_event_abilities(user)
     return if user.blank?
 
-    if user.account_owner?
-      can :manage, Event, company_id: user.company_id
-      can :display_calendar, Event, company_id: user.company_id
-    elsif user.hr?
-      can :manage, Event, company_id: user.company_id
-      can :display_calendar, Event, company_id: user.company_id
-    elsif user.department_head?
+    if user.account_owner? || user.hr? || user.department_head?
       can :manage, Event, company_id: user.company_id
       can :display_calendar, Event, company_id: user.company_id
     elsif user.employee?
