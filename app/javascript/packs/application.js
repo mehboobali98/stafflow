99eda0bcebe7d685d91b7fec894175458dbf122b
Mehboob Ali <FastNucesl174316>
2021-09-18 10:50:44 +0500

    fixed fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("channels")
 global.$ = global.jQuery = require("jquery"); 
 require("./settings")
 require("bootstrap")
+import "@fortawesome/fontawesome-free/css/all"
 document.addEventListener("turbolinks:load", function() {
   $(function () {
     $('[data-toggle="tooltip"]').tooltip();
