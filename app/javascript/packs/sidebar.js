da55239b5a59e46c85c5bd45b1c9d038d3085d0f
Mehboob Ali <FastNucesl174316>
2021-09-19 19:58:56 +0500

    sidebar internationalization done

--- app/javascript/packs/sidebar.js
+++ app/javascript/packs/sidebar.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
   $('#sidebar_toggle_btn').on('click', function () {
+    console.log("im in");
     $('.sidebar').toggleClass('active');
     if ($('.sidebar').hasClass('active')) {
+      console.log("active");
       $(".home_content").addClass("sidebar-toggle");
       $(".home_content").removeClass("home_content");
     } else {
+      console.log("inactive");
       $(".sidebar-toggle").addClass("home_content");
       $(".home_content").removeClass("sidebar-toggle");
     }
