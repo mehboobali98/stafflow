904d3df90b7fa54f11f4b0d515d3760a64df0d17
NadiaAhsan <nadia.ahsan@7vals.com>
2021-09-21 16:34:03 +0500

    profile complete

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,6 @@ Rails.application.routes.draw do
       get :display_companies
     end
   end
-      # get 'profile', to: 'users#profile'
 
   constraints subdomain: /^(?!www\Z)(\w+)/ do
     resources :members, controller: 'users'
