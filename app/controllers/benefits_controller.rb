4ea2d9f7a52fb91fff7256db1c1721b2a861d668
MShehryarKhan <shehryar.khan@7vals.com>
2021-09-22 13:12:44 +0500

    BreadCrumbs and Pagination on Benefits

--- app/controllers/benefits_controller.rb
+++ app/controllers/benefits_controller.rb
@@ -2,6 +2,7 @@
 
 class BenefitsController < ApplicationController
   before_action :load_benefit, only: %i[destroy edit update]
+  add_breadcrumb I18n.t('benefit.breadcrumbs.home'), :benefits_path
 
   # GET /benefits
   def index
@@ -13,6 +14,7 @@ class BenefitsController < ApplicationController
 
   # GET /benefits/new
   def new
+    add_breadcrumb t('benefit.breadcrumbs.new'), :new_benefit_path
     @benefit = Benefit.new
     respond_to do |format|
       format.html
@@ -21,6 +23,7 @@ class BenefitsController < ApplicationController
 
   # GET /benefits/:id/edit
   def edit
+    add_breadcrumb t('benefit.breadcrumbs.edit'), :edit_benefit_path
     respond_to do |format|
       format.html
     end
--- app/controllers/payrolls_controller.rb
+++ app/controllers/payrolls_controller.rb
@@ -5,8 +5,11 @@ class PayrollsController < ApplicationController
   before_action :load_payroll, only: :show
   before_action :load_payrolls, only: %i[index create]
   before_action :payroll_validation, only: :create
+  add_breadcrumb I18n.t('payroll.breadcrumbs.home'), :member_payrolls_path
+
   # GET members/:id/payrolls
   def index
+    @payrolls = @payrolls.paginate(page: params[:page], per_page: PAGE_SIZE)
     respond_to do |format|
       format.html
     end
@@ -14,6 +17,7 @@ class PayrollsController < ApplicationController
 
   # GET members/:id/payrolls/:id
   def show
+    add_breadcrumb @payroll.sequence_num, :member_payroll_path
     @applied_benefits = @payroll.applied_benefits.includes(:benefit)
     respond_to do |format|
       format.html
