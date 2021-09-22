11d5010530619aaef9e1078d735b9a924e9f5361
NadiaAhsan <nadia.ahsan@7vals.com>
2021-09-22 13:29:04 +0500

    done

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ApplicationMailer
   default from: 'testaccoun1717@gmail.com'
 
-  def approve_leave_information(user, emails)
+  def approve_leave_information(user, applied_from, applied_till, emails)
     @user = user
-
+    @applied_from = applied_from
+    @applied_till = applied_till
     mail to: user.email, subject: 'Approve Leave Information', cc: emails
   end
 end
