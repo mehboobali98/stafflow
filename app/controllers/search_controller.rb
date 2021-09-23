2355546e5e91c98bb8e54e4cfb04d1c4928119ca
NadiaAhsan <nadia.ahsan@7vals.com>
2021-09-23 18:09:00 +0500

    done

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def get_search_data
-        binding.pry
+    binding.pry
     result = Searchkick.search "nadia", models: [User, Department, Designation]
   end
 end
