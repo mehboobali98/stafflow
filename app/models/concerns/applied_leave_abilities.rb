2f1ddb470d1f785037d223463c7230dcdbd25ed2
Mehboob Ali <FastNucesl174316>
2021-09-23 19:53:48 +0500

    resolved

--- app/models/concerns/applied_leave_abilities.rb
+++ app/models/concerns/applied_leave_abilities.rb
@@ -1,19 +1,17 @@
 module AppliedLeaveAbilities
   def applied_leave_abilities(user)
-    if user.account_owner?
-      can %i[read all_applied_leaves], AppliedLeave, company_id: user.company_id
-    elsif user.hr?
+    if user.account_owner? || user.hr?
       can :manage, AppliedLeave, company_id: user.company_id
       cannot %i[update destroy], AppliedLeave, company_id: user.company_id
-      can %i[update destroy], AppliedLeave, company_id: user.company_id, state: %i[accepted rejected]
+      can %i[update destroy], AppliedLeave, company_id: user.company_id, state: :pending
     elsif user.department_head?
       can :manage, AppliedLeave, user: { department_id: user.department_id }
       cannot %i[update destroy], AppliedLeave, company_id: user.company_id
-      can %i[update destroy], AppliedLeave, company_id: user.company_id, state: %i[accepted rejected]
+      can %i[update destroy], AppliedLeave, company_id: user.company_id, state: :pending
     elsif user.employee?
       can %i[read create], AppliedLeave, company_id: user.company_id, user_id: user.id
       can %i[update destroy], AppliedLeave, company_id: user.company_id, user_id: user.id,
-                                            state: %i[accepted rejected]
+                                            state: :pending
     end
   end
 end
--- app/models/concerns/user_leave_abilities.rb
+++ app/models/concerns/user_leave_abilities.rb
@@ -5,7 +5,11 @@ module UserLeaveAbilities
       cannot :destroy, UserLeave do |user_leave|
         user_leave.applied_leaves.where(state: 'pending').exists?
       end
-    elsif user.department_head? || user.employee?
+    elsif user.department_head?
+      can :read, UserLeave do |user_leave|
+        user_leave.company_id == user.company_id && user_leave.user.department_id == user.department_id
+      end
+    elsif user.employee?
       can :read, UserLeave, company_id: user.company_id, user_id: user.id
     end
   end
