69a024337545d30d024e6d73fad703ecdc0ba149
MShehryarKhan <shehryar.khan@7vals.com>
2021-09-23 23:00:43 +0500

    temp

--- app/models/benefit.rb
+++ app/models/benefit.rb
@@ -7,8 +7,10 @@ class Benefit < ApplicationRecord
   belongs_to :company
   validates :name, uniqueness: true
   validates :name, presence: { message: I18n.t('benefit.validation.presence') }
-  validates :default_amount, presence: { message: I18n.t('users_benefit.validation.presence.') }, numericality: true
-  before_save :default_amount_zero?
+  validates :default_amount, presence: { message: I18n.t('users_benefit.validation.presence.') },
+                             numericality: { only_float: true, other_that: %(0),
+                                             message: I18n.t('benefit.validation.zero_check') }
+  # before_save :default_amount_zero?
 
   def default_amount_zero?
     return unless default_amount.zero?
