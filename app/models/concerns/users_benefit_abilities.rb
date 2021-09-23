415ecaf8db9c1777e3a4a08e80e235d820c8bbcd
MShehryarKhan <shehryar.khan@7vals.com>
2021-09-23 19:06:47 +0500

    temp

--- app/models/concerns/users_benefit_abilities.rb
+++ app/models/concerns/users_benefit_abilities.rb
@@ -1,6 +1,8 @@
 module UsersBenefitAbilities
   def define_users_benefit_abilities(user)
-    if user.employee? || user.department_head?
+    if user.employee?
+      can :read, UsersBenefit, id: user.id, company_id: user.company_id
+    elsif user.department_head?
       can :read, UsersBenefit, company_id: user.company_id
     elsif user.hr? || user.account_owner?
       can :manage, UsersBenefit, company_id: user.company_id
