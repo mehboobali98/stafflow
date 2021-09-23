4dd13ce09483f0d3d753230e34383a5a73272d5a
Mehboob Ali <FastNucesl174316>
2021-09-23 13:59:01 +0500

    spaces

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -3,12 +3,15 @@
 class AnalyticsController < ApplicationController
   before_action :authenticate_user!
 
+  # GET /analytics
   def analytics; end
 
+  # GET /analytics/employee_gender_distribution
   def employee_gender_distribution
     render json: @current_company.users.joins(:department).group(:gender, :name).count
   end
 
+  # GET /analytics/monthly_payroll
   def monthly_payroll
     @data = Date::MONTHNAMES.compact.map { |month| [month, rand(10_000..500_000)] }.to_h
     render json: @data
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,20 +3,24 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
 
+  # GET /dashboard
   def dashboard; end
 
+  # GET /dashboard/total_events
   def total_events
     respond_to do |format|
       format.json { render json: @current_company.events.size }
     end
   end
 
+  # GET /dashboard/employees_per_department
   def employees_per_department
     respond_to do |format|
       format.json { render json: @current_company.departments.joins(:users).group(:name).count }
     end
   end
 
+  # GET  /dashboard/employees_per_city
   def employees_per_city
     respond_to do |format|
       format.json { render json: @current_company.departments.joins(:users).group(:city).count }
