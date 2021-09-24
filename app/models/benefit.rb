8b0c3b69ef82a909f2e6fb9e68cd7fb8e159dfc8
MShehryarKhan <shehryar.khan@7vals.com>
2021-09-24 23:35:07 +0500

    benefit amount validation

--- app/models/benefit.rb
+++ app/models/benefit.rb
@@ -4,7 +4,7 @@ class Benefit < ApplicationRecord
   sequenceid :company, :benefits
   has_many :users_benefits, dependent: :restrict_with_error
   has_many :applied_benefits, dependent: :restrict_with_error
-  belongs_to :company # scope for company_id
+  belongs_to :company
   validates :name, uniqueness: true
   validates :name, presence: { message: I18n.t('benefit.validation.presence') }
   validates :name, format: { with: /\A[a-z A-Z]+\z/, message: I18n.t('benefit.validation.benefit_name') }
